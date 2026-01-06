b1ea8dd83cffe65d4f20e97a1cd3c47ab25ca5f6
mohammedali8371 <ali716796302@gmail.com>
2026-01-06 19:46:27 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8273832358:AAFpPUHBYIBBoKTLVzTxu6onU7yDRa4TmwI"
+OWNER_ID = 7958260008
+CHANNEL_USERNAME = "@lmh716"
+DEV_USERNAME = "@hhhbjvv"
+
+# رابط سيرفر تعديل الصور (API)
+IMAGE_API_URL = "https://your-server-url.com"  # هنا الرابط اللي هتشغله على Railway/Replit/Fly.io
